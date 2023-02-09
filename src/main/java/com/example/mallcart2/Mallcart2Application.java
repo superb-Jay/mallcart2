9d8cc08fff5b54e1e264bfa48149635155125fae
Jay <myride.jay@gmail.com>
2023-02-10 00:33:37 +0900

    initial commit

---
+++ src/main/java/com/example/mallcart2/Mallcart2Application.java
@@ -0,0 +1,13 @@
+package com.example.mallcart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mallcart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mallcart2Application.class, args);
+    }
+
+}
